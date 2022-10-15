7d616c9dcd2915374853ac08874915500c730394
Xavier <xadabunu@student.s19.be>
2022-10-15 22:35:16 +0200

    remove protec on c

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -16,9 +16,11 @@ char	*ft_strrchr(const char *s, int c)
 {
 	int	i;
 
-	if (!s || !c)
+	if (!s)
 		return (0);
 	i = ft_strlen(s);
+	if (c == 0)
+		return ((char *)s + i);
 	while (i >= 0)
 	{
 		if (s[i] == c)
