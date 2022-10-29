11846a448687bf72e66c9d80680af75de76969b9
xadabunu <xadabunu@student.s19.be>
2022-10-29 14:30:03 +0200

    corr negative len

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -26,25 +26,6 @@ static int	ft_isinset(char c, char const *set)
 	return (0);
 }
 
-/*static int	ft_trimlen(char const *str, char const *set)
-{
-	int	i;
-	int	ctr;
-
-	i = 0;
-	ctr = 0;
-	while (str[i] && ft_isinset(str[i], set))
-		++i;
-	while (str[i])
-	{
-		ctr++;
-		i++;
-	}
-	while (ctr > 0 && ft_isinset(str[--i], set))
-		--ctr;
-	return (ctr);
-}*/
-
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	size_t			len;
