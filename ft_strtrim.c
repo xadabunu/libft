cbd60cd1013e4b787a840e7bc3b9f8b421e37762
xadabunu <xadabunu@student.s19.be>
2022-11-02 16:29:47 +0100

    add protec

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -32,7 +32,7 @@ char	*ft_strtrim(char const *s1, char const *set)
 	unsigned int	pos;
 
 	if (!s1 || !set)
-		return (s1);
+		return ((char *)s1);
 	pos = 0;
 	while (ft_isinset(s1[pos], set))
 		++pos;
